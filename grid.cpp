63eb7ef1a38609781070da521d2cf8469212b187
Colin Heath <colinjhe@usc.edu>
2018-09-29 18:18:03 -0700

    Wrote final logic for programming assignment. Many thanks to whoever posted the P/D hint on piazza. Will keep thinking tonight and tomorrow about a more efficient way to solve (in terms of space).

--- grid.cpp
+++ grid.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <climits>
 //	Feel free to include any other C++ standard library if necessary.
 
 using namespace std;
@@ -32,24 +33,76 @@ int solve(int N, vector<vector<string> > G)
 	Return: the minimum life Brain needs to complete his task.
 */
 
-	//Declare an array of the same size as G. The array will hold structs to track the health needed at each tile.
-	TileHealth** requiredHealth = new TileHealth*[N];
-	for(int i = 0; i < N; i++)
+	//Declare an array with one more row and one more column than G.
+	//It's larger than G so that I can avoid edge case logic for the rightmost and bottommost tiles.
+	TileHealth** requiredHealth = new TileHealth*[N+1];
+	for(int i = 0; i < N+1; i++)
 	{
-		requiredHealth[i] = new TileHealth[N];
+		requiredHealth[i] = new TileHealth[N+1];
 
-		for(int j = 0; j < N; j++)
+		for(int j = 0; j < N+1; j++)
 		{
-			requiredHealth[i][j].noPowerUp = 0;
-			requiredHealth[i][j].onlyP = 0;
-			requiredHealth[i][j].onlyD = 0;
-			requiredHealth[i][j].bothPD = 0;
+			requiredHealth[i][j].noPowerUp = INT_MAX;
+			requiredHealth[i][j].onlyP = INT_MAX;
+			requiredHealth[i][j].onlyD = INT_MAX;
+			requiredHealth[i][j].bothPD = INT_MAX;
 		}
 	}
 
-	requiredHealth[N-1][N-1].noPowerUp = std::max(1, 1 - std::stoi(G[N-1][N-1]));
+	//These two entries MUST be one, so that the tile at [N-1][N-1] recognizes a need for a finishing health of 1.
+	requiredHealth[N][N-1].noPowerUp = 1;
+	requiredHealth[N][N-1].onlyP = 1;
+	requiredHealth[N][N-1].onlyD = 1;
+	requiredHealth[N][N-1].bothPD = 1;
 
-	//Fill out the rightmost column and bottommost row, for starters.
+	requiredHealth[N-1][N].noPowerUp = 1;
+	requiredHealth[N-1][N].onlyP = 1;
+	requiredHealth[N-1][N].onlyD = 1;
+	requiredHealth[N-1][N].bothPD = 1;
+
+	//Iterate in reverse through each row of the array, starting at (N-1, N-1), and fill in all four properties of the tile.
+	for(int i = N-1; i >= 0; i--)
+	{
+		for(int j = N-1; j >= 0; j--)
+		{
+			TileHealth rightTile = requiredHealth[i+1][j];
+			TileHealth downTile = requiredHealth[i][j+1];
+			//First, handle the Power-Up Case:
+			if(G[i][j] == "P")
+			{
+				requiredHealth[i][j].noPowerUp = std::min(rightTile.onlyP, downTile.onlyP);
+				requiredHealth[i][j].onlyP = std::min(rightTile.onlyP, downTile.onlyP);
+				requiredHealth[i][j].onlyD = std::min(rightTile.bothPD, downTile.bothPD);
+				requiredHealth[i][j].bothPD = std::min(rightTile.bothPD, downTile.bothPD);	
+			}
+			else if(G[i][j] == "D")
+			{
+				requiredHealth[i][j].noPowerUp = std::min(rightTile.onlyD, downTile.onlyD);
+				requiredHealth[i][j].onlyP = std::min(rightTile.bothPD, downTile.bothPD);
+				requiredHealth[i][j].onlyD = std::min(rightTile.onlyD, downTile.onlyD);
+				requiredHealth[i][j].bothPD = std::min(rightTile.bothPD, downTile.bothPD);	
+			}
+			else //Either malformed (which we were promised wouldn't happen) or integer
+			{
+				int tileDamage = std::stoi(G[i][j]);
+
+				requiredHealth[i][j].noPowerUp = std::max(1, std::min(rightTile.noPowerUp, downTile.noPowerUp) - tileDamage);
+				
+				if(tileDamage < 0) //If Guard
+				{
+					requiredHealth[i][j].onlyP = std::max(1, std::min(rightTile.noPowerUp, downTile.noPowerUp));
+					requiredHealth[i][j].onlyD = std::max(1, std::min(rightTile.onlyD, downTile.onlyD) - tileDamage);
+					requiredHealth[i][j].bothPD = std::max(1, std::min(rightTile.onlyD, downTile.onlyD));
+				}
+				else
+				{
+					requiredHealth[i][j].onlyP = std::max(1, std::min(rightTile.onlyP, downTile.onlyP) - tileDamage);
+					requiredHealth[i][j].onlyD = std::max(1, std::min(rightTile.noPowerUp, downTile.noPowerUp) - (2*tileDamage));
+					requiredHealth[i][j].bothPD = std::max(1, std::min(rightTile.onlyP, downTile.onlyP) - (2*tileDamage));
+				}
+			}
+		}
+	}
 
 	return requiredHealth[0][0].noPowerUp;
 }
